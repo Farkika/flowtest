9e2e6621473d9823b5386a5033b2c3b8701e6845
Dani <farkika39@gmail.com>
2020-06-27 12:29:07 +0200

    8. task

---
+++ 8.feladat.js
@@ -0,0 +1,20 @@
+const task = (n, m) => {
+  const arr = new Array(n);
+  for (let i = 0; i < n; i++) {
+    arr[i] = new Array(m);
+  }
+  let num = 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      arr[i][j] = num;
+      num *= 2;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      process.stdout.write(arr[i][j] + (j < arr[i].length - 1 ? ' ' : '\r\n'));
+    }
+  }
+};
+
+task(3, 3);
