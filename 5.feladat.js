446215b7d05ed6a60938f52a20737c7103dec31f
Dani <farkika39@gmail.com>
2020-06-27 12:06:09 +0200

    5. task

---
+++ 5.feladat.js
@@ -0,0 +1,12 @@
+const fgv = (num) => {
+  switch (num) {
+    case '1':
+      console.log('Az 1-est választottad!');
+      break;
+    case '2':
+      console.log('A 2-est választottad!');
+      break;
+    default:
+      console.log('Nyomd meg az 1-es vagy a 2-est');
+  }
+};
